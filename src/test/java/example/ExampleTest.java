0c63fe4408b6df9ecd3805cba77ebeb7c6019fd4
Sonali <sonali.burghate@gmail.com>
2019-02-27 22:28:01 +1100

    add two numbers function

--- src/test/java/example/ExampleTest.java
+++ src/test/java/example/ExampleTest.java
@@ -3,6 +3,7 @@ package example;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ExampleTest {
 
@@ -11,4 +12,8 @@ public class ExampleTest {
         assertEquals("Joined string didn't match", "1 2 3", Example.join("1", "2", "3"));
     }
 
+    @Test
+    public void testaddNumbers(){
+        assertTrue(Example.addNumbers(5 , 5) == 10);
+    }
 }
